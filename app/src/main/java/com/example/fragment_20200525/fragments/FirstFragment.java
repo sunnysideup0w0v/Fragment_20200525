c923adc4099fee0c0d1b7876cd169584b83468cf
toast eggs toast <59465490+sunnysideup0w0v@users.noreply.github.com>
2020-05-25 16:38:41 +0900

    fragment databinding  > + activityCreated에서 활용

--- app/src/main/java/com/example/fragment_20200525/fragments/FirstFragment.java
+++ app/src/main/java/com/example/fragment_20200525/fragments/FirstFragment.java
@@ -7,14 +7,31 @@ import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.databinding.DataBindingUtil;
 import androidx.fragment.app.Fragment;
 
 import com.example.fragment_20200525.R;
+import com.example.fragment_20200525.databinding.FragmentFirstBinding;
 
 public class FirstFragment extends Fragment {
+    FragmentFirstBinding binding;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_first, container);
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_first, container,false);
+        return binding.getRoot();
+    }
+// 나를 담고 있는 액티비티의 onCreate가 실행된 시점
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        // 액티비티의 onCreate처럼 여러가지 기능 작성
+        binding.okBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String input = binding.messageEdt.getText().toString();
+                binding.myTxt.setText(input);
+            }
+        });
     }
 }
