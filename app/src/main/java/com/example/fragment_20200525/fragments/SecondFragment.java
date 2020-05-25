87dfcb22f3062904bb8874a2529d0f7e385859d3
toast eggs toast <59465490+sunnysideup0w0v@users.noreply.github.com>
2020-05-25 17:09:56 +0900

    두번째 프래그먼트 연습문제  - getActivity 참조

---
+++ app/src/main/java/com/example/fragment_20200525/fragments/SecondFragment.java
@@ -0,0 +1,37 @@
+package com.example.fragment_20200525.fragments;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.databinding.DataBindingUtil;
+import androidx.fragment.app.Fragment;
+
+import com.example.fragment_20200525.R;
+import com.example.fragment_20200525.databinding.FragmentSecondBinding;
+
+public class SecondFragment extends Fragment {
+    FragmentSecondBinding binding;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_second, container, false);
+        return binding.getRoot();
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        binding.okBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getActivity(), "두 번째 Fragment 입니다.", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
